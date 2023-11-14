43235d8195a8398e98a573f480ba3f84dc072861
Magpie720 <doctor720.com@gmail.com>
2023-11-14 21:19:40 +0900

    update HW/backend

--- HW/HW.ino
+++ HW/HW.ino
@@ -2,80 +2,165 @@
 //#include <ArduinoJson.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <TinyGPS.h>
-#define LED_BUILTIN 2
+#include <TinyGPSPlus.h>
+#include "TYPE1SC.h"
+#include <U8x8lib.h>
+
+#define ID 1
+
+#define DebugSerial Serial
+#define M1Serial Serial2
+
+#define PWR_PIN 5
+#define RST_PIN 18
+#define WAKEUP_PIN 19
+#define EXT_ANT 4
+#define EXT_ANT_ON 0
+
+#define U8LOG_WIDTH 16
+#define U8LOG_HEIGHT 8
 
 const char* ssid = "";
 const char* password = "";
 const char* serverName = "http://34.82.193.43/update";
 const String key = "";
 
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
+uint8_t u8log_buffer[U8LOG_WIDTH * U8LOG_HEIGHT];
+U8X8LOG u8x8log;
+
+TYPE1SC TYPE1SC(M1Serial, DebugSerial, PWR_PIN, RST_PIN, WAKEUP_PIN);
+
 unsigned long lastTime = 0;
-unsigned long timerDelay = 2500;
-float lat, lon;
+unsigned long timerDelay = 5000;
+double lat, lon, speed, alt, course;
+uint32_t date, time_;
 bool flag = false;
 
-TinyGPS gps;
+char IPAddr[32];
+int _PORT = 80;
+char sckInfo[128];
+char recvBuffer[700];
+int recvSize;
+
+TinyGPSPlus gps;
+
+void extAntenna() {
+  if (EXT_ANT_ON) {
+    pinMode(EXT_ANT, OUTPUT);
+    digitalWrite(EXT_ANT, HIGH);
+    delay(100);
+  }
+}
 
 void setup() {
-  gps = TinyGPS();
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
-  Serial1.begin(9600, SERIAL_8N1, 33, 32);
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8log.begin(u8x8, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);
+  u8x8log.setRedrawMode(1); 
+  u8x8log.print("Initiating...\n");
 
-  WiFi.begin(ssid, password);
+  DebugSerial.begin(115200);
+  Serial1.begin(9600, SERIAL_8N1, 33, 32);
+  M1Serial.begin(115200);
+  
+  extAntenna();
+  gps = TinyGPSPlus();
+  TYPE1SC.init();
+  while (TYPE1SC.canConnect() != 0) {
+    u8x8log.print("LTE not ready\n");
+    delay(2000);
+  }
+
+  while (1) {
+    if (TYPE1SC.getIPAddr("34.82.193.43", IPAddr, sizeof(IPAddr)) == 0) break;
+    else {
+      u8x8log.print("IP address error\n");
+    }
+    delay(1000);
+  }
+  //TYPE1SC.socketCreate(1, IPAddr, _PORT);
+  //TYPE1SC.socketActivate();
+  
+  /*WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-  digitalWrite(LED_BUILTIN, LOW);
+  }*/
+
+  //Serial.println(WiFi.localIP());
+  u8x8.clear();
+  u8x8log.print("Ready.\n");
 }
 
 void getgps() {
-  gps.f_get_position(&lat, &lon);
-  Serial.print("Lat/Lon: ");
-  Serial.print(lat);
-  Serial.print(", ");
-  Serial.println(lon);
+  lat = gps.location.lat();
+  lon = gps.location.lng();
+  date = gps.date.value();
+  time_ = gps.time.value();
+  speed = gps.speed.kmph();
+  course = gps.course.deg();
+  alt = gps.altitude.meters();
 }
 
 void loop() {
   while (Serial1.available() > 0) {
     char c = Serial1.read();
-    //Serial.print(c);
     if (gps.encode(c)) {
-      Serial.println("New Pos Data");
-      flag = true;
-      getgps(); 
+      if (gps.location.isValid()){
+        flag = true;
+        getgps(); 
+      }
     }
   }
 
   if ((millis() - lastTime) > timerDelay) {
-    if(WiFi.status()== WL_CONNECTED){
-      if(flag == true) {
-        WiFiClient client;
-        HTTPClient http;
-        http.begin(client, serverName);
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-        String httpRequestData = "key="+key+"&lat="+String(lat, 6)+"&lon="+String(lon, 6);           
-        int httpResponseCode = http.POST(httpRequestData);
+    if(flag == true) {
+    /*if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      http.begin(client, serverName);
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      String httpRequestData = "key="+key+"&lat="+String(lat, 6)+"&lon="+String(lon, 6)+"&date="+String(date)+"&time="+String(time_)+"&speed="+String(speed)+"&course="+String(course)+"&alt="+String(alt);           
+      int httpResponseCode = http.POST(httpRequestData);
+      
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
         
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-          
-        http.end();
-        flag == false;
-      }
+      http.end();
+      flag == false;
     }
     else {
       Serial.println("WiFi Disconnected");
+    }*/
+
+      while (TYPE1SC.canConnect() != 0) {
+        u8x8log.print("LTE not ready\n");
+        delay(2000);
+      }
+      TYPE1SC.socketCreate(1, IPAddr, _PORT);
+      TYPE1SC.socketActivate();
+      String httpRequestData = "key="+key+"&id="+ID+"&lat="+String(lat, 6)+"&lon="+String(lon, 6)+"&date="+String(date)+"&time="+String(time_)+"&speed="+String(speed)+"&course="+String(course)+"&alt="+String(alt);   
+
+      String data = "POST /update HTTP/1.1\r\n";
+      data += "Host: 34.82.193.43\r\n";
+      data += "Content-Type:application/x-www-form-urlencoded\r\n";
+      data += "Content-Length:" + String(httpRequestData.length()) + "\r\n";
+      data += "\r\n" + httpRequestData;
+      
+      if (TYPE1SC.socketSend(data.c_str()) == 0) {
+        u8x8log.print("[POST Sent]");
+        u8x8log.print("\n");
+      } 
+      else {
+        u8x8log.print("HTTP send error");
+      }
+
+      TYPE1SC.socketDeActivate();
+      TYPE1SC.socketClose();
+
+      flag = false;
+      lastTime = millis();
     }
-    lastTime = millis();
   }
 }
--- web_page/index.js
+++ web_page/index.js
@@ -17,25 +17,44 @@ mongodb.MongoClient.connect(key.mongo, function(err, client){
   })
 })
 
+let coord01, coord02, coord03
+function getData(renderPage, res) {
+  db.collection('coordinate_01').find().toArray(function(err, result){
+    coord01 = result
+    db.collection('coordinate_02').find().toArray(function(err, result){
+      coord02 = result
+      db.collection('coordinate_03').find().toArray(function(err, result){
+        coord03 = result
+        res.render(renderPage, {coord01, coord02, coord03})
+      })
+    })
+  })
+  
+  
+}
 
 app.get('/', function(req, res) { 
   res.sendFile(__dirname +'/index.html')
 })
 
 app.get('/write', function(req, res) { 
-    res.sendFile(__dirname +'/write.html')
+  res.sendFile(__dirname +'/write.html')
 })
 
-app.get('/map', function(req, res) { 
-  db.collection('coordinate').find().toArray(function(err, result){
-    res.render('map.ejs', {coords : result})
-  })
+app.get('/main', function(req, res) { 
+  res.sendFile(__dirname +'/Main_page.html')
+})
+
+app.get('/map_line', function(req, res) { 
+  getData("map_line.ejs", res)
+})
+
+app.get('/map_setting', function(req, res) { 
+  getData("map_setting.ejs", res)
 })
 
 app.get('/list', function(req, res) {
-  db.collection('coordinate').find().toArray(function(err, result){
-    res.render('list.ejs', {coords : result})
-  })
+  getData("list.ejs", res)
 })
   
 app.post('/add', function(req, res){
@@ -45,7 +64,7 @@ app.post('/add', function(req, res){
         db.collection('config').findOne({name: 'count'}, function(err, result){
           var mycount = result.value;
           if(mycount >= 100) {res.render('alert', {msg: '등록된 데이터가 너무 많습니다.'}); return;}
-          db.collection('coordinate').insertOne( { lat : parseFloat(req.body.lat).toFixed(15), lon : parseFloat(req.body.lon).toFixed(15) } , function(){
+          db.collection('coordinate_01').insertOne( { lat : parseFloat(req.body.lat).toFixed(15), lon : parseFloat(req.body.lon).toFixed(15) } , function(){
             db.collection('config').updateOne({name: 'count'},{ $inc: {value: 1} })
             console.log("_id: " + (parseInt(mycount)+1) + " added")
             res.render('alert', {msg: (parseInt(mycount)+1) + "번 데이터를 추가하였습니다."});
@@ -60,7 +79,7 @@ app.post('/delete', function(req, res){
   let inPW = crypto.createHash('sha512').update(req.body.password).digest('base64');
   db.collection('config').findOne({name: 'password'}, function(err, result){
       if(result.value == inPW) {
-        db.collection('coordinate').deleteOne({_id: new mongodb.ObjectID(req.body.coordinate)}, function(err, result){
+        db.collection('coordinate_01').deleteOne({_id: new mongodb.ObjectID(req.body.coordinate)}, function(err, result){
           db.collection('config').updateOne({name: 'count'},{ $inc: {value: -1} });
           console.log("_id: " + req.body.coordinate + " deleted");
           res.render('alert', {msg: req.body.coordinate + " 데이터를 삭제하였습니다."});
@@ -70,25 +89,43 @@ app.post('/delete', function(req, res){
   })
 });
 
+function addData(collName, req, res) {
+  db.collection(collName).insertOne( 
+    { lat : parseFloat(req.body.lat).toFixed(6), lon : parseFloat(req.body.lon).toFixed(6), date : req.body.date, time : req.body.time, speed : req.body.speed, course : req.body.course, alt : req.body.alt },
+    function(){
+      //db.collection('config').updateOne({name: 'count'},{ $inc: {value: 1} });
+      //console.log(req.body.lat+', '+req.body.lon+" added");
+      prevLat = req.body.lat;
+      prevLon = req.body.lon;
+      res.status(201).send();
+    });
+}
+
 app.post('/update', function(req, res){
   db.collection('config').findOne({name: 'password'}, function(err, result){
-      if(result.value == req.body.key) {
-        //db.collection('config').findOne({name: 'count'}, function(err, result){
-          //var mycount = result.value;
-          //if(mycount >= 100) {res.status(401).send(); return;}
-          if(prevLat == req.body.lat && prevLon == req.body.lon) {res.status(200).send(); return;}
-          db.collection('coordinate').insertOne( { lat : parseFloat(req.body.lat).toFixed(6), lon : parseFloat(req.body.lon).toFixed(6) } , function(){
-            db.collection('config').updateOne({name: 'count'},{ $inc: {value: 1} });
-            console.log(req.body.lat+', '+req.body.lon+" added");
-            prevLat = req.body.lat;
-            prevLon = req.body.lon;
-            res.status(201).send();
-          });
-        //});
+    if(result.value == req.body.key) {
+      //db.collection('config').findOne({name: 'count'}, function(err, result){
+        //var mycount = result.value;
+        //if(mycount >= 100) {res.status(401).send(); return;}});
+      if(prevLat == req.body.lat && prevLon == req.body.lon) {res.status(200).send(); return;}
+      switch(req.body.id) {
+        case "1": 
+          addData("coordinate_01", req, res);
+          break;
+        case "2":
+          addData("coordinate_02", req, res);
+          break;
+        case "3":
+          addData("coordinate_03", req, res);
+          break;
+        default:
+          console.log("wrong id");
+          res.status(401).send();
       }
-      else {
-        console.log("wrong pw");
-        res.status(401).send();
-      }
+    }
+    else {
+      console.log("wrong pw");
+      res.status(401).send();
+    }
   })
 });
